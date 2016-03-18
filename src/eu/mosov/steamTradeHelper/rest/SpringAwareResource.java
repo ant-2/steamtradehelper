11bba8e3d95bb6cda8a5129821c6d6fa6f8b6cfd
ant <mosov.av@yandex.ru>
2016-03-19 03:44:25 +0500

    Spring added

---
+++ src/eu/mosov/steamTradeHelper/rest/SpringAwareResource.java
@@ -0,0 +1,26 @@
+package eu.mosov.steamTradeHelper.rest;
+
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.core.Context;
+
+/**
+ * Injects spring beans into fields which annotated with Spring annotations, like @Autowired and etc
+ */
+public abstract class SpringAwareResource {
+	@Context
+	public void setContext(ServletContext sc) {
+		autowire(sc);
+	}
+
+	public void autowire(ServletContext sc) {
+		autowire(sc, this);
+	}
+
+	public static void autowire(ServletContext sc, Object target) {
+		XmlWebApplicationContext context = (XmlWebApplicationContext) WebApplicationContextUtils.getWebApplicationContext(sc);
+		context.getBeanFactory().autowireBean(target);
+	}
+}
