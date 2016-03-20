e8813cf5f82744fee05b46db26ffd99b1240af3c
ant <mosov.av@yandex.ru>
2016-03-20 05:46:59 +0500

    added an client to access Backpack.tf API

---
+++ src/eu/mosov/steamTradeHelper/client/BackpackApiClient.java
@@ -0,0 +1,27 @@
+package eu.mosov.steamTradeHelper.client;
+
+import javax.json.JsonObject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+/**
+ * Makes requests to backpack.tf API
+ * */
+public class BackpackApiClient {
+	String CURRENCY = "http://backpack.tf/api/IGetCurrencies/v1/?key=56e4698ddea9e91a45b9de12";
+	String PRICES = "http://backpack.tf/api/IGetPrices/v4/?key=56e4698ddea9e91a45b9de12";
+
+	Client client;
+
+	public BackpackApiClient() {
+		client = ClientBuilder.newClient();
+	}
+
+	public JsonObject getCurrencies() {
+		return client.target(CURRENCY).request().get().readEntity(JsonObject.class);
+	}
+
+	public JsonObject getPrices() {
+		return client.target(PRICES).request().get().readEntity(JsonObject.class);
+	}
+}
