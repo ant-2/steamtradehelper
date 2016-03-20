e67e662d281a863052b87ad67db606a3363f2bac
ant <mosov.av@yandex.ru>
2016-03-20 06:20:58 +0500

    WebPricesRepository renamed to InMemoryDataRepository

---
+++ src/eu/mosov/steamTradeHelper/model/InMemoryDataRepository.java
@@ -0,0 +1,50 @@
+package eu.mosov.steamTradeHelper.model;
+
+import eu.mosov.steamTradeHelper.client.BackpackApiClient;
+
+import javax.json.JsonObject;
+import java.util.Date;
+
+public class InMemoryDataRepository implements DataRepository {
+
+	BackpackApiClient client = new BackpackApiClient();
+	Date lastTimeUpdated;
+	int updateInterval = 60 * 60 * 6 * 1000;	// updates data each 6 hours
+	JsonObject currencies;
+	JsonObject prices;
+
+	public InMemoryDataRepository() {
+		lastTimeUpdated = new Date();
+	}
+
+	@Override
+	public JsonObject getCurrencies() {
+		if (currencies == null || isTimeToUpdate()) {
+			currencies = client.getCurrencies();
+			setLastTimeUdated();
+		}
+		return currencies;
+	}
+
+	@Override
+	public JsonObject getPrices() {
+		if (prices == null || isTimeToUpdate()) {
+			prices = client.getPrices();
+			setLastTimeUdated();
+		}
+		return prices;
+	}
+
+	/**
+	 * Checks that were at least one day since last refresh
+	 * @return true if it is
+	 * */
+	boolean isTimeToUpdate() {
+		return System.currentTimeMillis() - lastTimeUpdated.getTime() > (updateInterval);
+	}
+
+	Date setLastTimeUdated() {
+		lastTimeUpdated.setTime(System.currentTimeMillis());
+		return lastTimeUpdated;
+	}
+}
--- src/eu/mosov/steamTradeHelper/model/PricesRepository.java
+++
@@ -1,7 +0,0 @@
-package eu.mosov.steamTradeHelper.model;
-
-public interface PricesRepository {
-	String getCurrencies();
-
-	String getPrices();
-}
