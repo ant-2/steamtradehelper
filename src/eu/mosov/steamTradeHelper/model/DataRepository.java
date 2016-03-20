c7d0378e868d6118be2d950dc5e1c3d8dcb97359
ant <mosov.av@yandex.ru>
2016-03-20 06:18:25 +0500

    PricesRepository renamed to DataRepository

---
+++ src/eu/mosov/steamTradeHelper/model/DataRepository.java
@@ -0,0 +1,12 @@
+package eu.mosov.steamTradeHelper.model;
+
+import javax.json.JsonObject;
+
+/**
+ * Repository to encapsulate data that retrieved via backpack.tf API
+ * */
+public interface DataRepository {
+	JsonObject getCurrencies();
+
+	JsonObject getPrices();
+}
