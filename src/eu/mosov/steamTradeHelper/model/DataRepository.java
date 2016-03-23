b9d732353917d2e0cc35f893b2c729c6fc06765c
ant <mosov.av@yandex.ru>
2016-03-23 05:51:17 +0500

    added method dropRefreshTimes()

---
+++ src/eu/mosov/steamTradeHelper/model/DataRepository.java
@@ -0,0 +1,14 @@
+package eu.mosov.steamTradeHelper.model;
+
+import javax.json.JsonObject;
+
+/**
+ * Repository to encapsulate data that retrieved via backpack.tf API
+ * */
+public interface DataRepository {
+	JsonObject getCurrencies();
+
+	JsonObject getPrices();
+	
+	void dropRefreshTimer();
+}
