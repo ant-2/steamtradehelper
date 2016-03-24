455fe4cfe680c40040891e58f7a1e838ec4a2475
ant <mosov.av@yandex.ru>
2016-03-24 05:25:18 +0500

    entity Item refactored, now there is only one class Item and no parent interface

--- src/eu/mosov/steamTradeHelper/entity/Item.java
+++ src/eu/mosov/steamTradeHelper/entity/Item.java
@@ -1,5 +1,25 @@
 package eu.mosov.steamTradeHelper.entity;
 
-public interface Item {
-	String getName();
+public class Item {
+	int id;
+	String name;
+
+	public Item(String name) {
+		this.name = name;
+	}
+
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof Item)) return false;
+		Item item = (Item) o;
+		return name.equals(item.name);
+	}
+
+	public int hashCode() {
+		return name.hashCode();
+	}
+
+	public String toString() {
+		return name;
+	}
 }
--- src/eu/mosov/steamTradeHelper/entity/parser/ParserImpl.java
+++ src/eu/mosov/steamTradeHelper/entity/parser/ParserImpl.java
@@ -1,7 +1,6 @@
 package eu.mosov.steamTradeHelper.entity.parser;
 
 import eu.mosov.steamTradeHelper.entity.Item;
-import eu.mosov.steamTradeHelper.entity.SimpleItem;
 
 import javax.json.JsonObject;
 import javax.json.JsonValue;
@@ -16,7 +15,7 @@ public class ParserImpl implements Parser<Item> {
 		List<Item> list = new ArrayList<>();
 		JsonObject curr = data.getJsonObject("response").getJsonObject("currencies");
 		for (Map.Entry<String, JsonValue> e : curr.entrySet()) {
-			list.add(new SimpleItem(e.getKey()));
+			list.add(new Item(e.getKey()));
 		}
 		return list;
 	}
@@ -26,7 +25,7 @@ public class ParserImpl implements Parser<Item> {
 		List<Item> list = new ArrayList<>();
 		JsonObject items = data.getJsonObject("response").getJsonObject("items");
 		for (Map.Entry<String, JsonValue> e : items.entrySet()) {
-			list.add(new SimpleItem(e.getKey()));
+			list.add(new Item(e.getKey()));
 		}
 		return list;
 	}
