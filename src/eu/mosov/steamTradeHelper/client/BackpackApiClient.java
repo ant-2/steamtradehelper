553ddae9d3455dc3adc110bbbe92ec806f3e5d97
ant <mosov.av@yandex.ru>
2016-03-30 15:26:00 +0500

    BackpackApiClient marked by Spring @Repository annotation for automatic discovery

--- src/eu/mosov/steamTradeHelper/client/BackpackApiClient.java
+++ src/eu/mosov/steamTradeHelper/client/BackpackApiClient.java
@@ -1,5 +1,7 @@
 package eu.mosov.steamTradeHelper.client;
 
+import org.springframework.stereotype.Repository;
+
 import javax.json.JsonObject;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -7,6 +9,7 @@ import javax.ws.rs.client.ClientBuilder;
 /**
  * Makes requests to backpack.tf API
  * */
+@Repository
 public class BackpackApiClient {
 	String CURRENCY = "http://backpack.tf/api/IGetCurrencies/v1/?key=56e4698ddea9e91a45b9de12";
 	String PRICES = "http://backpack.tf/api/IGetPrices/v4/?key=56e4698ddea9e91a45b9de12";
