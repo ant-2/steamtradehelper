6b7510ab55119d7ab35e5504e78b5438a1611837
ant <mosov.av@yandex.ru>
2016-03-30 15:30:42 +0500

    in InMemoryDataResource added feature to update data with data from Backpack.tf

--- src/eu/mosov/steamTradeHelper/model/InMemoryDataRepository.java
+++ src/eu/mosov/steamTradeHelper/model/InMemoryDataRepository.java
@@ -4,45 +4,45 @@ import eu.mosov.steamTradeHelper.client.BackpackApiClient;
 import eu.mosov.steamTradeHelper.entity.Item;
 import eu.mosov.steamTradeHelper.entity.parser.Parser;
 import eu.mosov.steamTradeHelper.entity.parser.ParserImpl;
-import org.springframework.stereotype.Repository;
 
 import javax.json.JsonObject;
 import java.util.Date;
 import java.util.List;
 
-@Repository
 public class InMemoryDataRepository implements DataRepository {
 	Parser<Item> parser;
 	BackpackApiClient client;
 	Date lastTimeUpdated;
-	int updateInterval = 60 * 60 * 6 * 1000;	// updates data each 6 hours
+	int updateInterval = 1000 * 60 * 60 * 6;	// updates data each 6 hours
 	JsonObject currencies;
+	List<Item> listCurr;
 	JsonObject prices;
-	int updateCount;
+	List<Item> listPrices;
 
 	public InMemoryDataRepository() {
 		client = new BackpackApiClient();
 		lastTimeUpdated = new Date();
-		parser = new ParserImpl();
+		parser = new ParserImpl<>();
 	}
 
 	@Override
 	public List<Item> getCurrencies() {
-		if (currencies == null) {
-			getCurrenciesAsJson();
+		if (listCurr == null || isTimeToUpdate()) {
+			listCurr = parser.parseCurrencies(client.getCurrencies());
+			setLastTimeUpdated();
 		}
-		return parser.parseCurrencies(this.currencies);
+		return listCurr;
 	}
 
 	@Override
 	public List<Item> getPrices() {
-		if (prices == null) {
-			getPricesAsJson();
+		if (listPrices == null || isTimeToUpdate()) {
+			listPrices = parser.parsePrices(client.getPrices());
+			setLastTimeUpdated();
 		}
-		return parser.parsePrices(this.prices);
+		return listPrices;
 	}
 
-	@Override
 	public JsonObject getCurrenciesAsJson() {
 		if (currencies == null || isTimeToUpdate()) {
 			currencies = client.getCurrencies();
@@ -51,7 +51,6 @@ public class InMemoryDataRepository implements DataRepository {
 		return currencies;
 	}
 
-	@Override
 	public JsonObject getPricesAsJson() {
 		if (prices == null || isTimeToUpdate()) {
 			prices = client.getPrices();
@@ -60,10 +59,21 @@ public class InMemoryDataRepository implements DataRepository {
 		return prices;
 	}
 
-	@Override
 	public DataRepository dropRefreshTimer() {
 		lastTimeUpdated.setTime(lastTimeUpdated.getTime() - updateInterval * 2);
 		return this;
+	}
+
+	public void updateBase() {
+		dropRefreshTimer();
+		getCurrenciesAsJson();
+		dropRefreshTimer();
+		getCurrencies();
+
+		dropRefreshTimer();
+		getPricesAsJson();
+		dropRefreshTimer();
+		getPrices();
 	}
 
 	/**
@@ -75,7 +85,6 @@ public class InMemoryDataRepository implements DataRepository {
 	}
 
 	Date setLastTimeUpdated() {
-		updateCount++;
 		lastTimeUpdated.setTime(System.currentTimeMillis());
 		return lastTimeUpdated;
 	}
--- test/eu/mosov/steamTradeHelper/model/InMemoryDataRepositoryTest.java
+++ test/eu/mosov/steamTradeHelper/model/InMemoryDataRepositoryTest.java
@@ -1,33 +1,55 @@
 package eu.mosov.steamTradeHelper.model;
 
+import eu.mosov.steamTradeHelper.client.BackpackApiClient;
+import eu.mosov.steamTradeHelper.entity.Item;
+import eu.mosov.steamTradeHelper.entity.parser.Parser;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
 
 public class InMemoryDataRepositoryTest {
 
-	InMemoryDataRepository repo;
+	InMemoryDataRepository repo = new InMemoryDataRepository();
 
 	@Before
-	public void setUp() {
-		repo = new InMemoryDataRepository();
+	public void initMocks() {
+		repo.client = mock(BackpackApiClient.class);
+		//noinspection unchecked
+		repo.parser = (Parser<Item>) mock(Parser.class);
 	}
 
 	@Test
 	public void actuallyGetCurrencies() {
-		repo.getCurrencies();
-		assertTrue(repo.currencies != null);
-		assertTrue(repo.updateCount == 1);
+		when(repo.parser.parseCurrencies(null)).thenReturn(singletonList(new Item("metal")));
+
+		List<Item> curr = repo.getCurrencies();
+		assertTrue(repo.listCurr != null);
+		assertThat(curr.size(), is (1));
+		assertThat(curr.get(0).getName(), is("metal"));
+	}
+
+	@Test
+	public void actuallyGetPrice() {
+		when(repo.parser.parsePrices(null)).thenReturn(singletonList(new Item("metal")));
+
+		List<Item> prices = repo.getPrices();
+		assertTrue(repo.listPrices != null);
+		assertThat(prices.size(), is(1));
+		assertThat(prices.get(0).getName(), is("metal"));
 	}
 
 	@Test
 	public void dataUpdatesOnlyAfterCertainAmountOfTime() {
 		repo.getCurrencies();
 		repo.getCurrencies();
-		assertTrue(repo.updateCount == 1);
+		verify(repo.client, times(1)).getCurrencies();
 	}
 	
 	@Test
@@ -35,11 +57,6 @@ public class InMemoryDataRepositoryTest {
 		repo.getCurrencies();
 		repo.dropRefreshTimer();
 		repo.getCurrencies();
-		assertTrue(repo.updateCount == 2);
-	}
-	
-	@Test
-	public void returnsAllCurrencies() {
-		assertThat(repo.getCurrencies().size(), is(4));
+		verify(repo.client, times(2)).getCurrencies();
 	}
 }
