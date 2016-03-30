0249fa5c9b1ac6c1e591d6ceb7b34d7b357ea2dc
ant <mosov.av@yandex.ru>
2016-03-30 15:20:14 +0500

    Item now annotation mapped as an JPA entity class

--- src/eu/mosov/steamTradeHelper/entity/Item.java
+++ src/eu/mosov/steamTradeHelper/entity/Item.java
@@ -1,8 +1,23 @@
 package eu.mosov.steamTradeHelper.entity;
 
+import javax.persistence.Access;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.AccessType.FIELD;
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity @Access(FIELD)
 public class Item {
-	int id;
+	@GeneratedValue(strategy = AUTO)
+	@Id Integer id;
+
 	String name;
+	String description;
+
+	protected Item() {
+	}
 
 	public Item(String name) {
 		this.name = name;
@@ -11,15 +26,35 @@ public class Item {
 	public boolean equals(Object o) {
 		if (this == o) return true;
 		if (!(o instanceof Item)) return false;
-		Item item = (Item) o;
-		return name.equals(item.name);
+
+		Item e = (Item) o;
+/*		if (id != null && e.id != null) {
+			return id.equals(e.id);
+		}
+		return name.equals(e.getName());*/
+		return name.equals(e.getName());
 	}
 
 	public int hashCode() {
+/*		int result = id != null ? id : 0;
+		result = 31 * result + (name != null ? name.hashCode() : 0);
+		return result;*/
 		return name.hashCode();
 	}
 
 	public String toString() {
+		return name+ " : "+description ;
+	}
+
+	public String getName() {
 		return name;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
 	}
 }
