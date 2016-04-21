45eb4871fcaba9784d69ab4a0e8aa9574c1e727c
ant <mosov.av@yandex.ru>
2016-04-21 20:51:48 +0500

    ItemParsedTest.java merged

--- test/eu/mosov/steamtradehelper/model/entity/parser/ItemParserTest.java
+++ test/eu/mosov/steamtradehelper/model/entity/parser/ItemParserTest.java
@@ -1,30 +1,47 @@
 package eu.mosov.steamtradehelper.model.entity.parser;
 
-import eu.mosov.steamtradehelper.JsonMocks;
 import eu.mosov.steamtradehelper.model.entity.Item;
+import eu.mosov.steamtradehelper.model.entity.Quality;
+import org.junit.Before;
 import org.junit.Test;
 
+import javax.json.Json;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
 import java.util.List;
 
-import static eu.mosov.steamtradehelper.JsonMocks.Mocks.CURRENCIES;
-import static eu.mosov.steamtradehelper.JsonMocks.Mocks.PRICES;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class ItemParserTest {
-	Parser<Item> parser = new ItemParser<>();
+  Parser<Item> parser = new ItemParser<>();
+  JsonBuilderFactory f = Json.createBuilderFactory(null);
+  JsonObject jsonToParse;
 
-	@Test
-	public void currenciesCorrectlyParsed() {
-		List<Item> items = parser.parse(JsonMocks.getMock(CURRENCIES));
-		assertThat(items.size(), is(1));
-		assertThat(items.get(0).getName(), is("metal"));
-	}
+  @Before
+  public void setUp() throws Exception {
+    jsonToParse = f.createObjectBuilder().add("response",
+        f.createObjectBuilder().add("items",
+            f.createObjectBuilder().add("itemName",
+                f.createObjectBuilder().add("prices",
+                    f.createObjectBuilder()
+                        .add("6", "null").add("11", "null")
+                )
+            )
+        )
+    ).build();
+  }
 
-	@Test
-	public void pricesCorrectlyParsed() {
-		List<Item> items = parser.parse(JsonMocks.getMock(PRICES));
-		assertThat(items.size(), is(1));
-		assertThat(items.get(0).getName(), is("metal"));
-	}
+  @Test
+  public void itemCorrectlyParsed() throws Exception {
+    List<Item> list = parser.parse(jsonToParse);
+
+    assertThat(list.size(), is(1));
+
+    Item item = list.get(0);
+    assertThat(item.getName(), is("itemName"));
+    assertThat(item.getQualities().size(), is(2));
+    assertThat(item.getQualities().contains(new Quality("Unique", 6)), is(true));
+    assertThat(item.getQualities().contains(new Quality("Strange", 11)), is(true));
+  }
 }
