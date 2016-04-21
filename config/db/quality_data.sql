acba931bb17c66f6be7e4ed62ea9633a28ee2294
ant <mosov.av@yandex.ru>
2016-04-21 21:17:53 +0500

    quality_data.sql reverted

--- config/db/quality_data.sql
+++ config/db/quality_data.sql
@@ -1,10 +1,10 @@
-insert into quality(quality) values
-('Unique'),
-('Strange'),
-('Decorated Weapon'),
-('Vintage'),
-('Genuine'),
-('Haunted'),
-('Collector''s'),
-('Normal'),
-('Self-Made');
+insert into quality values
+('Unique', 6),
+('Strange', 11),
+-- ('Decorated Weapon'),
+('Vintage', 3),
+('Genuine', 1),
+('Haunted', 13),
+('Collector''s', 14),
+('Normal', 0),
+('Self-Made', 9);
