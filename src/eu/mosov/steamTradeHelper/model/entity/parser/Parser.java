03939efb54981a52aefca91e0c3b3fa266ce90ef
ant <mosov.av@yandex.ru>
2016-05-03 05:36:39 +0500

    deleted all classes that's was relative to old domain model and this application purpose

--- src/eu/mosov/steamTradeHelper/model/entity/parser/Parser.java
+++
@@ -1,9 +0,0 @@
-package eu.mosov.steamtradehelper.model.entity.parser;
-
-import javax.json.JsonObject;
-import java.util.List;
-
-public interface Parser<T> {
-
-  List<T> parse(JsonObject data);
-}
