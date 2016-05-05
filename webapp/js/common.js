d33718e49abda777d3017dbdf658e86ebbcb8589
ant <mosov.av@yandex.ru>
2016-05-05 05:56:56 +0500

    refactored helper.html, did code clean up

--- webapp/js/common.js
+++ webapp/js/common.js
@@ -1,13 +1,3 @@
-// $(function () {
-//   $.ajaxSetup({
-//     error: function (req) {
-//       if (req.status == 0) return;
-//       console.log(req);
-//       alert('Failed: ' + req.status + ' ' + req.statusText + (req.responseText && req.responseText.length < 200 ? ': ' + req.responseText : ''));
-//     }
-//   });
-// });
-
 $(function () {
   $.ajaxSetup({
     error: function (req) {
@@ -17,3 +7,44 @@ $(function () {
     }
   });
 });
+
+function getResource(resourceUrl) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('get', resourceUrl, false);
+  xhr.send();
+
+  if (xhr.status != 200) {
+    console.log("Can't get resource: " + resourceUrl + ". " + xhr.status + ": " + xhr.statusText);
+  } else {
+    return xhr.responseText;
+  }
+}
+
+function getResourceAsync(resourceUrl) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('get', resourceUrl, true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4) {
+      if (xhr.status != 200) {
+        console.log("Can't get resource: " + resourceUrl + ". " + xhr.status + ": " + xhr.statusText);
+      } else {
+        return xhr.responseText;
+      }
+    }
+  };
+  xhr.send();
+}
+
+// Filters arr entries by string
+// ignores letter case
+function filterArrayBySubstring(arr, string) {
+  var matches = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (~arr[i]
+        .name.toLowerCase()
+        .indexOf(string.toLowerCase())) {
+      matches.push(arr[i]);
+    }
+  }
+  return matches;
+}
