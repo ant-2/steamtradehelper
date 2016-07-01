d485433d1f8ba702f2288698f64e8916ab9a12a7
ant <mosov.av@yandex.ru>
2016-07-01 22:07:50 +0500

    Change the project structure, divide all project files to two folders: frontend and backend
    Java files and realtive configuration files go to backend folder.
    Frontend relative files go to frontend folder.

--- bem-old/converter.js
+++
@@ -1,7 +0,0 @@
-var convertor = require('html2bemjson'),
-    file = ('pages/spreadsheet.html'),
-    fs = require('fs');
-
-(function (data) {
-    fs.writeFileSync('converted.bemjson.js', convertor.stringify(data));
-})(fs.readFileSync(file));
--- bem-old/model.blocks/ajax/ajax.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $.ajaxSetup({
-    error: function (req) {
-      if (req.status == 0) return;
-      console.log(req);
-      alert('Failed: ' + req.status + ' ' + req.statusText + (req.responseText && req.responseText.length < 200 ? ': ' + req.responseText : ''));
-    }
-  });
-});
